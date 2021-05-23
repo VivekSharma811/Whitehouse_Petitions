366dd642ed3252ab1ff9cd295a0919fbba755d88
VivekSharma811 <vs811861@gmail.com>
2021-05-23 14:06:07 +0530

    Multithreading added

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        performSelector(inBackground: #selector(fetchJson), with: nil)
+    }
+    
+    @objc func fetchJson() {
         let urlString: String
             
         if navigationController?.tabBarItem.tag == 0 {
@@ -21,18 +25,19 @@ class ViewController: UITableViewController {
         } else {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
+        
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parseJson(json: data)
             } else {
-                showError()
+                performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
             }
         } else {
-            showError()
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
     }
     
-    func showError() {
+    @objc func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Ok", style: .default))
         present(ac, animated: true)
@@ -43,7 +48,9 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
     }
     
